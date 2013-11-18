8d4067115b39e882cc000289191fca373842e487
Sellene <32432@alunos.isel.ipl.pt>
2013-11-18 16:15:46 +0000

    Visualization of coke commercial

---
+++ Application/VVS/VVS System/Controllers/VideoVisualizationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VVS_System.Controllers
+{
+    public class VideoVisualizationController : Controller
+    {
+        //
+        // GET: /VideoVisualition/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
