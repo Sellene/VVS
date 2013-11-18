127e0a7534accc6c02ea5f793d397de32ffb30d1
Sellene <32432@alunos.isel.ipl.pt>
2013-11-18 15:00:03 +0000

    First commit
    Project creation

---
+++ VSS System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VSS_System.Startup))]
+namespace VSS_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
