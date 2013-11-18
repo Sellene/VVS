95a71f3fde70d5707ea630b2ba73d09d04bd7a25
Sellene <32432@alunos.isel.ipl.pt>
2013-11-18 15:25:02 +0000

    Change project

---
+++ Application/VVS/VVS System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VVS_System.MvcApplication" Language="C#" %>
---
+++ Application/VVS/VVS System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VVS_System.Startup))]
+namespace VVS_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
